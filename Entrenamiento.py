a5c36d0c9b027a0c9cfcc6249fcfadf22f8c2630
vavralukas <68848771+vavralukas@users.noreply.github.com>
2022-05-16 15:02:13 +0200

    Update Entrenamiento.py

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,24 +1,24 @@
-# Creamos el modelo y lo entrenamos
-# from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 import tensorflow.python.keras.optimizers
 import tensorflow as tf
-# from tf.keras.models import Sequential
-# from tensorflow.python.keras.layers import Dropout, Flatten, Dense
-# from tensorflow.python.keras.layers import Convolution2D, MaxPooling2D
 from tensorflow.python.keras import backend as k
-# from tensorflow.python.keras.optimizers import Adam
 from tensorflow.python.keras.callbacks import TensorBoard
 
 k.clear_session()
 
+# FOLDERS WITH TRAIN SET AND VALIDATION SET
 datos_entrenamiento = "C:/Users/16pao/OneDrive/Escritorio/Teleco/Scbio/Flappymp/Fotos/entrenamiento"
 datos_validacion = "C:/Users/16pao/OneDrive/Escritorio/Teleco/Scbio/Flappymp/Fotos/validacion"
 
-iteraciones = 20
-altura, longitud = 200, 200
+# PARAMETERS OF TRAINING
+iteraciones = 200               # number of training iterations
+altura, longitud = 200, 200     # image size
 batch_size = 1
-pasos = 300/1
-pasos_validacion = 300/1
+pasos = 300/1                   # size of train set
+pasos_validacion = 300/1        # size of validation set
+clases = 2                       # number of classes model should recognize   
+lr = 0.0005                      # learning rate
+
+# parameters of filters
 filtrosconv1 = 32
 filtrosconv2 = 64
 filtrosconv3 = 128
@@ -26,24 +26,24 @@ tam_filtro1 = (4, 4)
 tam_filtro2 = (3, 3)
 tam_filtro3 = (2, 2)
 tam_pool = (2, 2)
-clases = 2
-lr = 0.0005                         # learning rate
 
+
+# PREPROCESSING 
+
+# train set - step 1
 preprocesamiento_entre = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale = 1./255,
     shear_range = 0.3,
     zoom_range = 0.3,
     horizontal_flip = True,
-    #vertical_flip=True,
-    #rotation_range=50,
-    #width_shift_range=0.3,                                          # Mover la foto a los lados
-    #height_shift_range=0.3
 )
 
+# validation set - step 1
 preprocesamiento_vali = tf.keras.preprocessing.image.ImageDataGenerator(
     rescale = 1./255,
 )
 
+# train set - step 2
 imagen_entreno = preprocesamiento_entre.flow_from_directory(
     datos_entrenamiento,
     target_size=(altura, longitud),
@@ -51,6 +51,7 @@ imagen_entreno = preprocesamiento_entre.flow_from_directory(
     class_mode="categorical",
 )
 
+# validation set - step 2
 imagen_validacion = preprocesamiento_entre.flow_from_directory(
     datos_validacion,
     target_size=(altura, longitud),
@@ -58,8 +59,10 @@ imagen_validacion = preprocesamiento_entre.flow_from_directory(
     class_mode="categorical",
 )
 
+# CREATING NEURAL NETWORK
 cnn = tf.keras.models.Sequential()
 
+# adding convolutional and pooling layers
 cnn.add(tf.keras.layers.Convolution2D(filtrosconv1, tam_filtro1, padding="same", input_shape=(altura, longitud, 3), activation="relu"))
 cnn.add(tf.keras.layers.MaxPooling2D(pool_size=tam_pool))
 cnn.add(tf.keras.layers.Convolution2D(filtrosconv2, tam_filtro2, activation="relu"))
@@ -67,19 +70,20 @@ cnn.add(tf.keras.layers.MaxPooling2D(pool_size=tam_pool))
 cnn.add(tf.keras.layers.Convolution2D(filtrosconv3, tam_filtro3, activation="relu"))
 cnn.add(tf.keras.layers.MaxPooling2D(pool_size=tam_pool))
 
+# additional layers
 cnn.add(tf.keras.layers.Flatten())
 cnn.add(tf.keras.layers.Dense(640, activation="relu"))
 cnn.add(tf.keras.layers.Dropout(0.5))
 cnn.add(tf.keras.layers.Dense(clases, activation="softmax"))
 
+# optimizing model
 optimizar = tf.keras.optimizers.Adam(learning_rate=lr)
 cnn.compile(loss = "categorical_crossentropy", optimizer=optimizar, metrics=["accuracy"])
 BoardCNN2 = TensorBoard(log_dir="C:/Users/16pao/OneDrive/Escritorio/Teleco/Scbio/Flappymp")
+
+# TRAINING MODEL
 cnn.fit(imagen_entreno, steps_per_epoch=pasos, callbacks=BoardCNN2, epochs=iteraciones, validation_data=imagen_validacion, validation_steps=pasos)
 
+# SAVE TRAINED MODEL AND WEIGHTS
 cnn.save("Modelo.h5")
 cnn.save_weights("pesos.h5")
-
-# C:\Users\16pao>C:\Users\16pao\OneDrive\Escritorio\Teleco\Scbio\Flappymp\venv\Scripts\tensorboard.exe --logdir C:\Users\16pao\OneDrive\Escritorio\Teleco\Scbio\Flappymp
-
-
