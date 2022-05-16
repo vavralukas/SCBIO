3f07ce34d0a009975e41c5e2306a39552704b899
vavralukas <68848771+vavralukas@users.noreply.github.com>
2022-05-16 14:47:32 +0200

    Update Manos.py

--- Manos.py
+++ Manos.py
@@ -1,75 +1,94 @@
-# LIBRERIAS
-import cv2                  # libreria de Opencv-python
-import os                   # movernos entre carpetas
+# LIBRARIES
+import cv2                  
+import os                   
 import mediapipe as mp
 import numpy as np
 import time
-# Detector de manos
 
-# Creamos carpetas para almacenar las fotos de entrenamiento y validacion
-nombre = "palma"
-directorio = "C:/Users/16pao/OneDrive/Escritorio/Teleco/Scbio/Flappymp/Fotos/validacion"
+
+# CREATE PATH TO FOLDERS WHERE PHOTOS WILL BE SAVED
+nombre = "palm"
+directorio = "C:/Users/lukas/Documents/SCBIO/training_set"
 carpeta = directorio + "/" + nombre
 
-# por si no esta creada la carpeta que se cree sola
+# CREATE FOLDER IF IT DOES NOT EXIST YET
 if not os.path.exists(carpeta):
     print("Carpeta creada:", carpeta)
     os.makedirs(carpeta)
 
-# contador para el nombre de las fotos
-cont = 100
+# LOOP COUNTER (also used as an input where creating photos' names)
+cont = 1
 
-# Iniciamos la camara (0 para WebCam 1 para externa)
+# INITIATE CAMERA
 cap = cv2.VideoCapture(0)
 
-# Objeto que almacena deteccion y seguimiento de las manos
-
-clase_manos = mp.solutions.hands            # detector de manos de Google
+# CREATE OBJECT WHICH DETECTS HANDS AND THEIR MOVEMENTS
+clase_manos = mp.solutions.hands            
 manos = clase_manos.Hands()
 
-# dibuja los puntos cardinales de la manos
+# SHOW CARDINAL POINTS OF DETECTED HANDS
 dibujo = mp.solutions.drawing_utils
 
+# MAIN LOOP - TAKING IMAGES OF HANDS
 while True:
-    ret, frame = cap.read()                                     # lectura de camara
-    color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)              # Opencv entrega los colores en BGR y queremos RGB
-    copia = frame.copy()                                        # copia para procesamiento posterior
-    resultado = manos.process(color)
-    posiciones = []                                             # coordenadas de los puntos
+    ret, frame = cap.read()                                     # read frames from the camera
+    color = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)              # convert BGR frame to RGB frame
+    copia = frame.copy()                                        # copy of the frame for future processing
+    resultado = manos.process(color)                            # final frame from camera to be processed
+    posiciones = []                                             # array of hand mark coordinates necessary for image processing
 
-    if resultado.multi_hand_landmarks:                          # si detecto alguna mano
+    # IF CONDITION - HAND IS DETECTED?
+    if resultado.multi_hand_landmarks:      
+
+        # LOOP FOR EVERY HAND DETECTED                    
         for mano in resultado.multi_hand_landmarks:
+
+            # SAVE AND DRAW HAND MARK COORDINATES 
             for id, lm in enumerate(mano.landmark):
                 alto, ancho, c = frame.shape
                 corx, cory = int(lm.x*ancho), int(lm.y*alto)
                 posiciones.append([id,corx,cory])
-                dibujo.draw_landmarks(frame, mano, clase_manos.HAND_CONNECTIONS)
+                dibujo.draw_landmarks(frame, mano, clase_manos.HAND_CONNECTIONS) # display conncetions between marks 
 
+            # IF HAND MARK COORDINATES WERE SAVED
             if len(posiciones) != 0:
-                pto_i1 = posiciones[4]
-                pto_i2 = posiciones[20]
-                pto_i3 = posiciones[12]
-                pto_i4 = posiciones[0]
-                pto_i5 = posiciones[9]
-                pto_5 = posiciones[5]
-                pto_17 = posiciones[17]
+                # coordinates used for further image processing
+                pto_central = posiciones[9]                     # mark in the centre of the hand
+                pto_5 = posiciones[5]                           # auxiliary variable to determine the size of the final frame
+                pto_17 = posiciones[17]                         # auxiliary variable to determine the size of the final frame
+
+                # determining the size/coordinates of the final frame which is saved
                 distancia = int(np.round_(np.sqrt((pto_5[1] - pto_17[1])**2 + (pto_5[2] - pto_17[2])**2)))
-                #print(distancia)
-                x1, y1 = (pto_i5[1]-int(1.5*distancia)), (pto_i5[2]-int(1.8*distancia))
-                ancho, alto = (x1+3*distancia), (y1+int(3.5*distancia))
-                x2, y2 = ancho, alto
-                dedos_reg = copia[y1:y2, x1:x2]
+                x1, y1 = (pto_central[1]-int(1.5*distancia)), (pto_central[2]-int(1.8*distancia))
+                x2, y2 = (x1+3*distancia), (y1+int(3.5*distancia))    
+
+                # conditions to prevent errors when frame leaves camera
+                if y1 < 0:                                       
+                    y1 = 0
+                if x1 < 0:
+                    x1 = 0
+
+                # cut final frame according to determined coordinates
+                final_frame = copia[y1:y2, x1:x2]
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-            dedos_reg = cv2.resize(dedos_reg, (3*distancia, int(3.5*distancia)), interpolation= cv2.INTER_CUBIC)
-            dedos_reg = cv2.resize(dedos_reg, (200, 200))
-            cv2.imwrite(carpeta+"/palma_{}.jpg".format(cont), dedos_reg)
+
+            # resize and save frame as jpg to elected folder     
+            final_frame = cv2.resize(final_frame, (3*distancia, int(3.5*distancia)), interpolation= cv2.INTER_CUBIC)
+            final_frame = cv2.resize(final_frame, (200, 200))
+            cv2.imwrite(carpeta+"/"+nombre+"_{}.jpg".format(cont), final_frame)
+
+            # increase counter
             cont = cont + 1
             time.sleep(0.05)
 
+    # DISPLAY CAMERA 
     cv2.imshow("Video", frame)
-    k = cv2.waitKey(1)
-    if k == 27 or cont >= 200:       # Cuando tengamos guardadas 300 fotos
+
+    # TERMINATE LOOP
+    k = cv2.waitKey(1)          
+    if k == 27 or cont > 100:       # terminate loop if ESC is pressed or 100 photos have been taken
         break
 
+# CLOSE CAMERA
 cap.release()
 cv2.destroyAllWindows()
